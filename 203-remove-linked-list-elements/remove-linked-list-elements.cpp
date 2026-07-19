bcefa7147270539459c41da53825b6967f8eda31
badalmbandana-cell <badalmbandana@gmail.com>
2026-07-19 10:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (47.07%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* curr=head;
+        ListNode* dummy=new ListNode;
+        ListNode* prev=dummy;
+        dummy->next=head;
+        while(curr!=NULL){
+            if (curr->val==val){
+                prev->next=curr->next;
+                curr=curr->next;
+            }
+            else{
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
